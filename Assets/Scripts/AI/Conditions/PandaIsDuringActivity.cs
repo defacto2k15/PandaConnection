ac88be4c7add2580781ddbfeca71793670f24c70
defacto2k15 <bczerwinski462@gmail.com>
2020-02-01 16:08:28 +0100

    Added activity flag

---
+++ Assets/Scripts/AI/Conditions/PandaIsDuringActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Scripts.Utils;
+using Pada1.BBCore;
+using Pada1.BBCore.Framework;
+using UnityEngine;
+
+namespace Assets.Scripts.AI.Conditions
+{
+    [Condition("MyCondition/PandaIsDuringActivity")]
+    public class  PandaIsDuringActivityCondition: ConditionBase
+    {
+        [InParam("ThisPanda")]
+        GameObject ThisPanda;
+
+        public override bool Check()
+        {
+            var container = ThisPanda.GetComponentNotNull<PandaActivityFlag>();
+            return container.IsDuringActivity;
+        }
+    }
+}
