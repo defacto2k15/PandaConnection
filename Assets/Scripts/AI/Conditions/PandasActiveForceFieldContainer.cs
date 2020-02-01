209e9a128d0eadee7df5da72eb7f2378ccf16d6f
defacto2k15 <bczerwinski462@gmail.com>
2020-02-01 15:29:45 +0100

    small fixes

--- Assets/Scripts/AI/Conditions/PandasActiveForceFieldContainer.cs
+++ Assets/Scripts/AI/Conditions/PandasActiveForceFieldContainer.cs
@@ -35,21 +35,27 @@ namespace Assets.Scripts.AI.Conditions
             }
         }
 
-        public bool IsInActiveFieldArea => _activeForceFields.Any();
+        public bool IsInActiveFieldArea => _activeForceFields.Any() &&
+                                           _activeForceFields.Select(c =>
+                                               Mathf.Abs(AttractionModifiedByDistanceFactor(c, (c.WorldSpacePosition - transform.position).magnitude))).Sum() > 0.1f;
+
+        private float AttractionModifiedByDistanceFactor(IForceField c, float distance)
+        {
+
+            var distanceFactor = Mathf.InverseLerp(c.Radius + 3, 0, distance);
+            return (distanceFactor * c.RetriveAttraction(_panda));
+        }
 
         public Vector3 ForceVector
         {
             get
             {
                 Assert.IsTrue(_activeForceFields.Any());
-                return( _activeForceFields.Select(c =>
+                return (_activeForceFields.Select(c =>
                 {
                     var positionDelta = c.WorldSpacePosition - transform.position;
-
-                    var distanceFactor = Mathf.InverseLerp(c.Radius+3, 0, positionDelta.magnitude);
-                    return (distanceFactor * c.RetriveAttraction(_panda)) * positionDelta.normalized;
+                    return AttractionModifiedByDistanceFactor(c, positionDelta.magnitude) * positionDelta;
                 }).Aggregate(Vector3.zero, (a, b) => a + b) / _activeForceFields.Count).normalized;
-
             }
         }
     }
