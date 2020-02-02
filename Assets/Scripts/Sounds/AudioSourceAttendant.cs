f66b2a50a55591d5667783456a464ff62135fb0c
defacto2k15 <bczerwinski462@gmail.com>
2020-02-02 12:40:02 +0100

    Added initial sound files

---
+++ Assets/Scripts/Sounds/AudioSourceAttendant.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Assets.Scripts.Sounds
+{
+    public class AudioSourceAttendant : MonoBehaviour
+    {
+        public SoundType Type;
+        public bool IsBackground;   
+        public bool IsSustained;
+        public GameObject PlayingObject;
+
+        private float _timerResetMoment;
+        private float _timerLength;
+
+        void Awake()
+        {
+            if (IsSustained)
+            {
+                _timerLength = 1;
+                StartCoroutine(TimerLoop());
+            }
+        }
+
+        public void StopAndDestroy()
+        {
+            GameObject.Destroy(gameObject);
+        }
+
+        public void SustainTimer(float timerLength)
+        {
+            Assert.IsTrue(IsSustained);
+            _timerLength = timerLength;
+            _timerResetMoment = Time.time;
+        }
+
+        private IEnumerator TimerLoop()
+        {
+            while (Time.time - _timerResetMoment < _timerLength)
+            {
+                yield return new WaitForSeconds(_timerLength);
+            }
+            StopAndDestroy();
+        }
+    }
+}
---
+++ Assets/Scripts/Sounds/SoundManager.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Scripts.Utils;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Assets.Scripts.Sounds
+{
+    public class SoundManager : MonoBehaviour
+    {
+        public SoundManager instance;
+        public List<AudioSourceAttendant> Prefabs;
+
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+        }
+
+        public void PlayOneShotSound(SoundType type)
+        {
+            var clipWithType = Prefabs.Single(c => c.Type == type);
+            Assert.IsTrue(!clipWithType.IsBackground);
+            Assert.IsTrue(!clipWithType.IsSustained);
+
+            Instantiate(clipWithType, transform);
+        }
+
+        public void PlaySustainedTheme(GameObject playingObject, SoundType type, float playTimer)
+        {
+            var clipWithType = Prefabs.Single(c => c.Type == type);
+            Assert.IsTrue(!clipWithType.IsBackground);
+            Assert.IsTrue(clipWithType.IsSustained);
+
+            var similarPlayingObjects = GetComponentsInChildren<AudioSourceAttendant>()
+                .Where(c => c.Type == type && playingObject.Equals(c.PlayingObject)).ToList();
+            Assert.IsTrue(similarPlayingObjects.Count <= 1);
+
+            AudioSourceAttendant attendant;
+            if (similarPlayingObjects.Any())
+            {
+                attendant = similarPlayingObjects.Single();
+            }
+            else
+            {
+                attendant = Instantiate(clipWithType, transform).GetComponentNotNull<AudioSourceAttendant>();
+                attendant.PlayingObject = playingObject;
+            }
+
+            attendant.SustainTimer(playTimer);
+        }
+
+        public void PlayBackgroundTheme(SoundType type)
+        {
+            var clipWithType = Prefabs.Single(c => c.Type == type);
+            Assert.IsTrue(clipWithType.IsBackground);
+            Assert.IsTrue(!clipWithType.IsSustained);
+
+            var currentBackgrounds = GetComponentsInChildren<AudioSourceAttendant>().ToList();
+            Assert.IsTrue(currentBackgrounds.Count <=1 );
+
+            if (currentBackgrounds.Count == 1)
+            {
+                currentBackgrounds.Single().StopAndDestroy();
+            }
+
+            Instantiate(clipWithType, transform);
+        }
+    }
+
+    public enum SoundType
+    {
+        MenuAcceptBig,
+        BackgroundTheme,
+        Dying,
+        Eating,
+        MenuClick,
+        OpeningTheme,
+        Sex,
+        Walking,
+        Yay
+    }
+}
