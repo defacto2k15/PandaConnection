26f588e3ff021d3eb6a092b1f186163eda314b94
Anna Sierhej <as@jugglergames.com>
2020-02-02 09:20:00 +0100

    Poczatek menu

---
+++ Assets/Scripts/Menu/MenuCanvas.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuCanvas : MonoBehaviour
+{
+    public SceneField menuScene;
+    [Header("Start game scenes")]
+    public SceneField startGameAIScene;
+    public SceneField startGameUIScene;
+    [Header("Middle game scenes")]
+    public SceneField middleGameAIScene;
+    public SceneField middleGameUIScene;
+
+    public GameObject tutorialObject;
+
+    public Button startGameButton;
+    public Button middleGameButton;
+    public Button tutorialButton;
+
+    private void Awake()
+    {
+        startGameButton.onClick.AddListener(StartGame);
+        middleGameButton.onClick.AddListener(StartMiddle);
+        tutorialButton.onClick.AddListener(StartTutorial);
+    }
+
+    private void StartGame()
+    {
+        StartCoroutine(UnloadScene());
+    }
+
+    private IEnumerator UnloadScene()
+    {
+        SceneManager.LoadScene(startGameUIScene, LoadSceneMode.Additive);
+        SceneManager.LoadScene(startGameAIScene, LoadSceneMode.Additive);
+        yield return null;
+        SceneManager.UnloadSceneAsync("MenuScene");
+    }
+
+
+    private void StartMiddle()
+    {
+        SceneManager.UnloadSceneAsync("MenuScene");
+        SceneManager.LoadScene(middleGameAIScene, LoadSceneMode.Additive);
+        SceneManager.LoadScene(middleGameUIScene, LoadSceneMode.Additive);
+    }
+
+    private void StartTutorial()
+    {
+        tutorialObject.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/SceneField.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SceneField : ISerializationCallbackReceiver
+{
+
+#if UNITY_EDITOR
+    public UnityEditor.SceneAsset sceneAsset;
+#endif
+
+#pragma warning disable 414
+    [SerializeField, HideInInspector]
+    public string sceneName = "";
+#pragma warning restore 414
+
+    // Makes it work with the existing Unity methods (LoadLevel/LoadScene)
+    public static implicit operator string(SceneField sceneField)
+    {
+#if UNITY_EDITOR
+        return System.IO.Path.GetFileNameWithoutExtension(UnityEditor.AssetDatabase.GetAssetPath(sceneField.sceneAsset));
+#else
+        return sceneField.sceneName;
+#endif
+    }
+
+    public void OnBeforeSerialize()
+    {
+#if UNITY_EDITOR
+        sceneName = this;
+#endif
+    }
+    public void OnAfterDeserialize() { }
+}
